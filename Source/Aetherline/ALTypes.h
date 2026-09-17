dbec6e7879124fbbe784c044fecf8ef8cefad61b
kam11s <kameronevans11s@gmail.com>
2026-09-17 12:56:08 -0500

    Add EALDropPhase and BattleRoyale to ALTypes.h

--- Source/Aetherline/ALTypes.h
+++ Source/Aetherline/ALTypes.h
@@ -3,23 +3,52 @@
 #include "ALTypes.generated.h"
 
 UENUM(BlueprintType)
-enum class EALHero : uint8 { Wraith, Bastion, Pulse, Volt, Grav, Ember };
+enum class EALHero : uint8
+{
+	Wraith,
+	Bastion,
+	Pulse,
+	Volt,
+	Grav,
+	Ember
+};
+
 UENUM(BlueprintType)
-enum class EALPlaylist : uint8 { Training, BotSkirmish, QuickPlay, Ranked };
+enum class EALPlaylist : uint8
+{
+	Training,
+	BotSkirmish,
+	QuickPlay,
+	Ranked,
+	BattleRoyale
+};
+
 UENUM(BlueprintType)
-enum class EALTeam : uint8 { Ally, Enemy, None };
+enum class EALDropPhase : uint8
+{
+	Lobby,
+	OnDropship,
+	Skydiving,
+	Grounded
+};
+
+UENUM(BlueprintType)
+enum class EALTeam : uint8
+{
+	Ally,
+	Enemy,
+	None
+};
 
 USTRUCT(BlueprintType)
 struct FALHeroDef
 {
 	GENERATED_BODY()
 	UPROPERTY(EditAnywhere, BlueprintReadOnly) EALHero Id = EALHero::Wraith;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly) FName DisplayName = TEXT("Wraith");
+	UPROPERTY(EditAnywhere, BlueprintReadOnly) FString DisplayName;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly) float MaxHealth = 200.f;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly) float MoveSpeed = 600.f;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly) float FireRate = 8.f;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly) float Damage = 18.f;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly) float Range = 20000.f;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly) float Spread = 1.2f;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly) FLinearColor Color = FLinearColor(0.1f, 0.7f, 0.9f);
 };
